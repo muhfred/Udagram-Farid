14a48ac419fb818550a4dc59f32c8a7d06cdad8a
muhfred <muhfred@outlook.com>
2021-07-04 22:51:21 +0200

    all services with changes to the implementation.

---
+++ Udagram-feed-service/src/controllers/v0/feed/routes/feed.router.ts
@@ -0,0 +1,98 @@
+import { Router, Request, Response } from 'express';
+import { FeedItem } from '../models/FeedItem';
+import { requireAuth } from '../../users/routes/auth.router';
+import * as AWS from '../../../../aws';
+
+const router: Router = Router();
+
+// Get all feed items
+router.get('/', async (req: Request, res: Response) => {
+    const items = await FeedItem.findAndCountAll({ order: [['id', 'DESC']] });
+    items.rows.map((item) => {
+        if (item.url) {
+            item.url = AWS.getGetSignedUrl(item.url);
+        }
+    });
+    res.status(200).send(items);
+});
+
+// @TODO
+// Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const item = await FeedItem.findByPk(id);
+    if (item === null) {
+        res.status(404).send({ message: 'not found!' });
+    } else {
+        item.url = AWS.getGetSignedUrl(item.url);
+        res.status(200).send(item);
+    }
+});
+
+// update a specific resource
+router.patch('/:id',
+    requireAuth,
+    async (req: Request, res: Response) => {
+        // @TODO try it yourself
+        const id = req.params.id;
+        const { caption, url } = req.body;
+
+        const item = await FeedItem.findOne({
+            where: { id: id },
+            attributes: ['id', 'caption', 'url']
+        });
+        if (!item) {
+            res.status(404).send({ message: 'Not Found!' });
+        } else {
+            const updatedItem = await item.update({ caption: caption, url: url }, {
+                where: {
+                    id: id
+                }, returning: true
+            });
+            updatedItem.dataValues.url = AWS.getGetSignedUrl(updatedItem.dataValues.url);
+            res.status(200).send(updatedItem.dataValues);
+        }
+
+    });
+
+
+// Get a signed url to put a new item in the bucket
+router.get('/signed-url/:fileName',
+    requireAuth,
+    async (req: Request, res: Response) => {
+        const { fileName } = req.params;
+        const url = AWS.getPutSignedUrl(fileName);
+        res.status(201).send({ url: url });
+    });
+
+// Post meta data and the filename after a file is uploaded
+// NOTE the file name is they key name in the s3 bucket.
+// body : {caption: string, fileName: string};
+router.post('/',
+    requireAuth,
+    async (req: Request, res: Response) => {
+        const caption = req.body.caption;
+        const fileName = req.body.url;
+
+        // check Caption is valid
+        if (!caption) {
+            return res.status(400).send({ message: 'Caption is required or malformed' });
+        }
+
+        // check Filename is valid
+        if (!fileName) {
+            return res.status(400).send({ message: 'File url is required' });
+        }
+
+        const item = new FeedItem({
+            caption: caption,
+            url: fileName
+        });
+
+        const saved_item = await item.save();
+
+        saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+        res.status(201).send(saved_item);
+    });
+
+export const FeedRouter: Router = router;
---
+++ Udagram-image-filter-service/src/util/util.ts
@@ -0,0 +1,49 @@
+import { reject } from 'bluebird';
+import fs from 'fs';
+import Jimp = require('jimp');
+import fetch from 'node-fetch';
+// filterImageFromURL
+// helper function to download, filter, and save the filtered image locally
+// returns the absolute path to the local image
+// INPUTS
+//    inputURL: string - a publicly accessible url to an image file
+// RETURNS
+//    an absolute path to a filtered image locally saved file
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+        const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+        const file = await download(inputURL, outpath);
+        Jimp.read(file)
+            .then(photo => {
+                photo
+                    .resize(256, 256) // resize
+                    .quality(60) // set JPEG quality
+                    .greyscale() // set greyscale
+                    .write(__dirname + outpath, (img) => {
+                        resolve(__dirname + outpath);
+                    });
+            }).catch(err => {
+                console.error(err);
+                reject("Could not read image.");
+            });
+    });
+}
+
+// Get image from url and save to local folder.
+async function download(url: string, outpath: string) {
+    const response = await fetch(url);
+    const buffer = await response.buffer();
+    fs.writeFile(__dirname + outpath, buffer, () =>
+        console.log('finished downloading!'));
+    return __dirname + outpath;
+}
+// deleteLocalFiles
+// helper function to delete files on the local disk
+// useful to cleanup after tasks
+// INPUTS
+//    files: Array<string> an array of absolute paths to files
+export async function deleteLocalFiles(files: Array<string>) {
+    for (let file of files) {
+        fs.unlinkSync(file);
+    }
+}
